1372f02f8b0fa4cb58c096e02b6392a0b1b0e2f1
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-08-27 17:39:40 +0300

    first draft

---
+++ 02_Haastavampi_tehtävä/varipeli.js
@@ -0,0 +1,10 @@
+/* 1. Luo HTML-tiedosto, jossa on seuraavat elementit: 
+- Painike "Aloita peli",joka aloittaa pelin ja järjestää laatikot satunnaisesti. 
+- Alue, johon laatikot sijoitetaan. 
+- Viisi värilaatikkoa, joita voi vetää ja pudottaa.
+
+2.Luo erillinen JavaScript-tiedosto, jossa: 
+-Määritellään tapahtumakäsittelijä painikkeelle, joka järjestää laatikot satunnaisesti. 
+-Toteutetaan drag & drop-toiminnallisuus, jotta käyttäjä voi vetää ja pudottaa laatikoita oikeaan
+järjestykseen.
+*/
