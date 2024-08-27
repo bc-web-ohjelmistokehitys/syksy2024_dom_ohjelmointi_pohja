e437bb160f069b58467370ef7fef237337c7b113
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2024-08-27 17:43:03 +0300

    second draft

--- vastaukset/basic_DOM_1/script.js
+++
@@ -1,9 +0,0 @@
-/* 
-Write JavaScript code that selects content id by using getElementById.
-Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
-*/
-
-const changeText = () => {
-  const textPlacement = document.getElementById('content');
-  textPlacement.textContent = 'Hello World!';
-};
